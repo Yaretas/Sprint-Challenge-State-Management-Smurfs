074617bdebe4e5d138607ed3c691b88f943dd903
Yara Castro <yaracastro24@gmail.com>
2019-10-14 21:57:45 -0500

    Setting up Axios

---
+++ smurfs/src/components/SmurfDisplay.js
@@ -0,0 +1,10 @@
+import React from "react";
+import SmurfData from "./smurfData";
+
+export default function SmurfDisplay(props){
+    return(
+        <div>
+            <h2>{props.name}</h2>
+        </div>
+    )
+}
